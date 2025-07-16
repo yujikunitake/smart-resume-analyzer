9a40b635dd8a6c4faded0a9063c307a60ddedc5d
Yuji Miranda Kunitake <andrekuni123@gmail.com>
2025-07-15 23:31:52 -0300

    tests: complementar testes de rota para caso com query

--- tests/test_routers_analyze.py
+++ tests/test_routers_analyze.py
@@ -1,10 +1,32 @@
-# tests/test_router_analyze.py
+# tests/test_routers_analyze.py
 from unittest.mock import patch
 from fastapi.testclient import TestClient
 from app.main import app
 import uuid
 
 client = TestClient(app)
+
+
+@patch("app.services.ocr.extract_text")
+def test_analyze_with_query(mock_ocr):
+    mock_ocr.return_value = "Texto extraído simulado para consulta."
+
+    with open("tests/assets/sample_resume.pdf", "rb") as fake_file:
+        files = [("files", ("resume.pdf", fake_file, "text/plain"))]
+        data = {
+            "request_id": str(uuid.uuid4()),
+            "user_id": "fabio",
+            "query": "Qual experiência profissional?"
+        }
+
+        response = client.post("/analyze/", files=files, data=data)
+        assert response.status_code == 200
+
+        json_data = response.json()
+        assert "resume.pdf" in json_data
+        # Qdo tem query, o retorno será o texto extraído
+        assert json_data["resume.pdf"] == ("Texto extraído simulado para"
+                                           " consulta.")
 
 
 @patch("app.services.summarizer.summarize_text")
